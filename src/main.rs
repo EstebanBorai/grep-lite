5ac5b24db9fdf95107037b6aa68d8b9e23245de5
Esteban Borai <estebanborai@gmail.com>
2021-08-23 21:06:39 -0400

    feat: store lines into a vector

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Line {
+    index: usize,
+    value: String,
+}
+
+impl Line {
+    pub fn new(index: usize, value: String) -> Self {
+        Line { index, value }
+    }
+}
+
 fn main() {
     let term = "something";
     let input = "\
@@ -8,8 +19,11 @@ fn main() {
         historians as having marked Harrison's ascendancy as a composer to
         the level of the Beatles' principal songwriters, John Lennon and
         Paul McCartney.";
+    let mut lines: Vec<Line> = Vec::with_capacity(10);
 
     for (line_num, line) in input.lines().enumerate() {
+        lines.push(Line::new(line_num, line.to_string()));
+
         if line.to_lowercase().contains(term) {
             println!("{}: {}", line_num, line);
         }
